ce9eed9b2490e3989f68460a52ed90d2fafb7cd0
Florian Kunneman <fkunnema@red.uvt.nl>
2018-11-07 09:25:07 +0100

    first steps

---
+++ gvsim_demo/GVSim/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class GvsimConfig(AppConfig):
+    name = 'GVSim'
---
+++ gvsim_demo/GVSim/urls.py
@@ -0,0 +1,8 @@
+
+from django.conf.urls import url
+
+from .views import PrikbordHome
+
+urlpatterns = [
+    url(r'^$', GVHome.as_view(), name='gvhome'),
+]
